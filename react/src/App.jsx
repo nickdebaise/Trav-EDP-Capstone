47904e1f8bab804b98e056b3a487fd6bbe493f22
Eric Jara <ejara@travelers.com>
2025-07-09 14:30:49 -0400

    fixing login router

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -2,14 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Login from './components/Login'
-import Search from './components/Search'
 import {
   BrowserRouter as Router,
   Route,
   Routes,
   Link
 } from "react-router-dom";
+import Login from './components/Login';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -36,9 +35,11 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Router>
       <Routes>
-        <Route path="/Login" element={<Login />} />
+        <Route path="/Login" element={<Login/>} />
       </Routes>
+      </Router>
     </>
   )
 }
--- react/src/components/Login.jsx
+++ react/src/components/Login.jsx
@@ -1,30 +1,49 @@
 import React, { useState } from 'react';
-import { useAuth } from '../hooks/AuthContext';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 
-function LoginForm() {
+export default function LoginForm() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    const { login } = useAuth();
     const navigate = useNavigate();
 
     const handleLogin = async (event) => {
         event.preventDefault();
-        await login(username, password);
-        console.log(username, password);
-        navigate('/add');
+        fetch(`${import.meta.env.VITE_API_URL}/login`, {
+            method: "POST",
+            body: JSON.stringify({
+                "name": username,
+                "password": password
+            }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            // TODO: Handle the response data
+            if(data.ok){
+                alert(data.message);
+                navigate('/employee');
+            }
+        })
+        .catch((error) => {
+            // Handle any errors
+            console.error(error);
+        });
+        console.log("Login sent successfully");
+        
     };
 
     return (
         <div className="container">
             <form onSubmit={handleLogin} className="mt-5">
                 <div className="form-group">
-                    <label htmlFor="username">Username</label>
+                    <label htmlFor="username">Name</label>
                     <input
                         type="text"
                         className="form-control"
                         id="username"
-                        placeholder="Enter username"
+                        placeholder="Enter Full Name"
                         value={username}
                         onChange={(e) => setUsername(e.target.value)}
                     />
@@ -42,8 +61,10 @@ function LoginForm() {
                 </div>
                 <button type="submit" className="btn btn-primary">Login</button>
             </form>
+            <span>
+                Don't have an account?&nbsp; 
+                <Link to="/register">Register!</Link>
+            </span>
         </div>
     );
 }
-
-export default LoginForm;
