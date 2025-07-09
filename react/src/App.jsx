73ed5222ddb8f26ddcc7b6c8811947d3d3274220
Eric Jara <ejara@travelers.com>
2025-07-09 14:10:19 -0400

    adding some routes and component files

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -2,6 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Login from './components/Login'
+import Search from './components/Search'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link
+} from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +36,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Routes>
+        <Route path="/Login" element={<Login />} />
+      </Routes>
     </>
   )
 }
