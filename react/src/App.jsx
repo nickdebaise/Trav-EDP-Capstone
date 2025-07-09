5158740440f6c510a22769bd9b3b5285baac1ed3
Eric Jara <ejara@travelers.com>
2025-07-09 15:15:22 -0400

    adding what i can , cant connect to routes yet

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,49 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState , useEffect} from 'react';
+import './index.css';
+import "./App.css"
+import Employee from './components/Employee';
+import Register from './components/Register'
+import Navbar from './components/Navbar';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 import {
   BrowserRouter as Router,
   Route,
-  Routes,
-  Link
+  Routes
 } from "react-router-dom";
 import Login from './components/Login';
-import Register from './components/Register';
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
 
+  const [employee, setEmployee] = useState({})
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <Router>
+    <Router>
+      <Navbar fixed="top" setEmployee={setEmployee}/>
       <Routes>
-        <Route path="/Login" element={<Login/>} />
-        <Route path="/register" element={<Register/>}/>
+        <Route exact path="/" element={
+          <Login />
+        }/>
+        <Route path="/employee" element={
+          <Employee data={employee} setEmployee={setEmployee}/>
+        }/>
+        <Route path="/login" element={
+          <Login />
+        }/>
+        <Route path="/register" element={
+          <Register/>
+        }/>
       </Routes>
-      </Router>
-    </>
+    </Router>
   )
 }
-
-export default App
